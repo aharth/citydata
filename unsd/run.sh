175b49cd6198c4e45dc163c6aff58ce7f5f6f200
Andreas Harth <harth@kit.edu>
2017-08-02 22:15:25 +0200

    added city file

--- unsd/run.sh
+++ unsd/run.sh
@@ -1,3 +1,7 @@
 tarql.sh unsd-methodology.rq > tmp.ttl
-rapper -i turtle tmp.ttl -o turtle  | sed -e "s/file:\/\/.*/> ./" > unsd-methodology.ttl
+rapper -i turtle tmp.ttl -o turtle  | sed -e "s/file:\/\/.*/#> ./" > unsd-methodology.ttl
+rm tmp.ttl
+
+tarql.sh 240.rq > tmp.ttl
+cwm -n3 tmp.ttl > cities.ttl
 rm tmp.ttl
