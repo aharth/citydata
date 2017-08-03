0a4dbacec0b9295b24fc597ce32e3b417aa67c73
Andreas Harth <harth@kit.edu>
2017-08-03 16:55:24 +0200

    more wikidata links

--- wikidata/run.sh
+++ wikidata/run.sh
@@ -1,2 +1,4 @@
 # get only one link (lowest Wikidata URI)
 sparql "https://query.wikidata.org/sparql" wikidata-eurostat-geo.rq wikidata-eurostat-geo.rdf
+
+tarql.sh eurostat-cities-links.rq > eurostat-cities-links.ttl
