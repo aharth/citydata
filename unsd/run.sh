7f4d8162af0f80f6cfa2483f743e5e4144edeccc
Andreas Harth <harth@kit.edu>
2017-08-03 17:21:13 +0200

    fixed uris across files

--- unsd/run.sh
+++ unsd/run.sh
@@ -8,6 +8,6 @@ rm tmp.ttl
 
 tarql.sh 240-proper.rq > tmp.ttl
 cwm --n3 tmp.ttl --ntriples > tmp.nt
-rapper -i ntriples tmp.nt -o turtle | sed -e "s/http:\/\/harth.org\/andreas\/2017\/citydata\/unsd\///" | sed -e "s/#C/#000001/" | sed -e "s/#U/#000002/"> undata-cities-proper-links.ttl
+rapper -i ntriples tmp.nt -o turtle | sed -e "s/http:\/\/harth.org\/andreas\/2017\/citydata\/unsd\///" | sed -e "s/#C/#000001/" | sed -e "s/#U/#000002/"> cities-proper-links.ttl
 rm tmp.ttl
 rm tmp.nt
--- wikidata/run.sh
+++ wikidata/run.sh
@@ -1,4 +1,4 @@
 # get only one link (lowest Wikidata URI)
-sparql "https://query.wikidata.org/sparql" wikidata-eurostat-geo.rq wikidata-eurostat-geo.rdf
+sparql "https://query.wikidata.org/sparql" eurostat-geo-links.rq eurostat-geo-links.rdf
 
 tarql.sh eurostat-cities-links.rq > eurostat-cities-links.ttl
